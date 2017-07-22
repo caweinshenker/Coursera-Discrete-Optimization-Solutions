f362e1eaaf2e90063caef9877de468c1cb0f1d5a
Colin Weinshenker <caweinshenker@email.wm.edu>
2017-07-22 02:06:40 -0700

    Begin adjacency-matrix based solution to graph coloring

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,35 +1,100 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+import numpy as np
+from utils import parse_input, build_graph
+
+
+
+
+
+def solve_it_trivial(node_count, edge_count, edges):
+    """Return a trivial solution: every node gets its own color
+
+    Parameters
+    ---------
+    node_count -- number of nodes
+    edge_count -- number of edges
+    edges      -- list (e_i1, e_i2) tuples representing edges
+
+    Returns
+    -------
+    optimal     -- is this a proven optimal solution
+    output_data -- string formatting of the solution as specified in the handout
+    """
+    optimal = 0
+    solution = range(0, node_count)
+    return (optimal, solution)
+
+
+def solve_it_nontrivial(node_count, edge_count, edges):
+    """
+    """
+    #Create the adjacency matrix representing the graph
+    optimal = 1
+    graph = build_graph(node_count, edge_count, edges)
+
+    #Get the cardinalities sorted with corresponding row indices
+    cardinalities = np.sum(graph, axis=1)
+    cardinalities = [(i, int(cardinalities[i])) for i in range(len(cardinalities))]
+    cardinalities.sort(key=lambda x: -x[1])
+
+    #Create the colors array
+    colors = [0] * node_count
+    visited = [0] * node_count
+
+    #Print out any information
+    #print(edges)
+    #print(graph)
+    #print(colors)
+    #print(cardinalities)
+
+
+    #Begin with the most connected node
+    #to follow first-fail principle
+    for index, node in enumerate(cardinalities):
+        row = node[0]
+        visited[row] = 1
+
+        #Get the list of unvisited neighbors
+        #and sort their visitation order by their cardinality
+        neighbors = [col for col in range(node_count) if graph[row, col] == 1 and visited[col] == 0]
+        neighbors.sort(key = lambda x: np.sum(graph[x:]))
+
+        #For each neighbor, find the maximal color of that neighbor's neighborss
+        #and assign the neighbor max_color + 1
+        for neighbor in neighbors:
+            neighbors_neighbors = [col for col in range(node_count) if graph[neighbor, col] == 1]
+            max_color = max([colors[neighbors_neighbor] for neighbors_neighbor in neighbors_neighbors])
+            colors[neighbor] = max_color + 1
+
+        #After updating for a particular node, print the resulting colors array
+        print("Update result")
+        print(row, neighbors, colors)
+    print("Solution: " + str(colors))
+    return (optimal, colors)
+
+
+
+
 
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
-    # parse the input
-    lines = input_data.split('\n')
+    node_count, edge_count, edges = parse_input(input_data)
 
-    first_line = lines[0].split()
-    node_count = int(first_line[0])
-    edge_count = int(first_line[1])
-
-    edges = []
-    for i in range(1, edge_count + 1):
-        line = lines[i]
-        parts = line.split()
-        edges.append((int(parts[0]), int(parts[1])))
-
-    # build a trivial solution
-    # every node has its own color
-    solution = range(0, node_count)
+    #optimal, solution = solve_it_trivial(node_count, edge_count, edges)
+    optimal, solution = solve_it_nontrivial(node_count, edge_count, edges)
 
     # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
+    output_data = str(len(set(solution))) + ' ' + str(optimal) + '\n'
     output_data += ' '.join(map(str, solution))
 
     return output_data
 
 
-import sys
+
 
 if __name__ == '__main__':
     import sys
@@ -40,4 +105,3 @@ if __name__ == '__main__':
         print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
-
---
+++ coloring/utils.py
@@ -0,0 +1,54 @@
+"""
+Utilities for the graph coloring problem
+
+"""
+
+from node import Node
+import numpy as np
+
+
+
+
+def parse_input(input_data):
+    """Parse the input data for the graph
+
+    Parameters
+    ----------
+    input_data -- the raw data from the file of the form
+                  |V|    |E|
+                  e_11   e_22
+                  ...
+                  e_|E|1 e_|E|2
+
+    Return
+    ------
+    node_count -- number of nodes
+    edge_count -- number of edges
+    edges      -- list (e_i1, e_i2) tuples representing edges
+    """
+    # parse the input
+    lines = input_data.split('\n')
+
+    first_line = lines[0].split()
+    node_count = int(first_line[0])
+    edge_count = int(first_line[1])
+
+    edges = []
+    for i in range(1, edge_count + 1):
+        line = lines[i]
+        parts = line.split()
+        edges.append((int(parts[0]), int(parts[1])))
+
+    return (node_count, edge_count, edges)
+
+
+def build_graph(node_count, edge_count, edges):
+    """
+    Build an adjacency matrix representation of the graph from the edges
+
+    """
+    adjacencyMatrix = np.zeros((node_count, node_count))
+    for edge in edges:
+        adjacencyMatrix[edge[0], edge[1]] = 1
+        adjacencyMatrix[edge[1], edge[0]] = 1
+    return adjacencyMatrix
